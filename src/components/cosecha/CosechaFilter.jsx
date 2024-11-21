036648ca865e58827fe6a805e126057c8d58ef3d
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-11-20 23:38:08 -0500

    Cambiar el tamaño del botón buscar en Cosecha

--- src/components/cosecha/CosechaFilter.jsx
+++ src/components/cosecha/CosechaFilter.jsx
@@ -64,7 +64,8 @@ const CosechaFilter = ({onFiltersValue}) => {
                 </div>
                 
                 <button 
-                    className="bg-[#313395] text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded-lg shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-[#313395] text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded-lg shadow hover:shadow-lg outline-none 
+                    focus:outline-none mr-1 mt-6 md:w-28 ease-linear transition-all duration-150 "
                     onClick={sendDataToParent}>
                     Buscar
                 </button>
