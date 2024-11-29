1ac42e61fbc32831f7bcf4752d2267ed5a5ffe47
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-11-29 01:11:44 -0500

    Agregar la API Tierras Disponibles

--- src/services/tierra.js
+++ src/services/tierra.js
@@ -34,6 +34,33 @@ export const searchTierras = async({search}) => {
       throw new Error('Error al buscar tierras')
     }
   }
+export const searchTierrasAvailable = async() => {
+  try {
+    const response = await fetch(`${appSetting.apiUrl}Tierra/Available`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const tierras = await response.json()
+    const formatter= tierras?.map(tierra => ({
+      id : tierra.tierraId,
+      uc : tierra.tierraUc,
+      campo : tierra.tierraCampo,
+      ha : tierra.tierraHa,
+      sector : tierra.tierraSector,
+      valle : tierra.tierraValle,
+      activo : tierra.tierraStatus
+    })) 
+    return formatter
+  } catch (error) {
+    console.log('searchTierrasAvailable:', error.message)
+    throw new Error('Error al buscar tierras disponibles')
+  }
+}
 
   export const tierraGetById = async({id}) => {
     try {
