b4e749d2fc69f717d7ab941b7f43c248f09e4c1b
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-12-01 17:58:03 -0500

    Nuevas opciones en el Sidebar: Ticket y Corte

--- src/components/mocks/SIDEBAR_ITEMS.jsx
+++ src/components/mocks/SIDEBAR_ITEMS.jsx
@@ -1,4 +1,4 @@
-import { BarChart2, Home, MapIcon, RadarIcon, Users } from "lucide-react"
+import { AlignHorizontalDistributeCenter, BarChart2, Home, MapIcon, RadarIcon, TicketCheck, Users, Waves } from "lucide-react"
 export const SIDEBAR_ITEMS = [
     {
         name:"Principal", icon:Home, color:"#6366f1", href:"/"
@@ -20,6 +20,18 @@ export const SIDEBAR_ITEMS = [
     },
     {
         name:"Cosecha", icon:BarChart2, color:"#6366f1", href:"/cosecha"
+    },
+    { 
+      name: "Ticket", 
+      href: "/ticket", 
+      icon: TicketCheck, 
+      color: "#6366f1" 
+    },
+    { 
+      name: "Corte", 
+      href: "/corte", 
+      icon: AlignHorizontalDistributeCenter, 
+      color: "#6366f1" 
     },
     // { 
     //   name: "Reportes", 
