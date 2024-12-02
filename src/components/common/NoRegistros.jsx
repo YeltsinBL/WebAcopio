366d9da5bd42931c7d3b6df6dec50b8296d0d857
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-12-02 02:55:25 -0500

    Componente: No hay registros

---
+++ src/components/common/NoRegistros.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NoRegistros = () => {
+  return (
+    <tr>
+	  <td colSpan={5} className="text-center py-4">
+		No hay registros
+	  </td>
+	</tr>
+  )
+}
