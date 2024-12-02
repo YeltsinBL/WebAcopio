b6e09a685b021816e692dbf3c8237f85ac960b0c
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-12-02 14:20:55 -0500

    Creación del Modal Delete

---
+++ src/components/common/ModalDelete.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ModalDelete = ({title, message, children}) => {
+  return (
+    <>
+      <div
+          className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+        >
+        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+          {/*content*/}
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            {/*header*/}
+            <div className="flex items-start justify-center p-5 border-b border-solid border-blueGray-200 rounded-t">
+              <h3 className="text-3xl font-bold text-[#313395]">
+                {title}
+              </h3>
+            </div>
+            {/*body*/}
+            <p className="justify-center text-black p-6" >{message}</p>
+            {/*footer*/}
+            {children}
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </>
+  )
+}
+
+export default ModalDelete
