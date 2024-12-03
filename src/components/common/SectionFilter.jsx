c16e106bb534201aed37579d58c5f3286ca0d483
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-12-03 12:34:46 -0500

    Custom Sección de Filtrado

---
+++ src/components/common/SectionFilter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SectionFilter = ({children}) => {
+  return (
+    <div className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700 mb-8'>
+		<form action="">
+            <div className='grid grid-cols-1 md:grid-cols-5 gap-4 '>
+            {children}
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default SectionFilter
