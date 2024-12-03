82f2a96122cd05d90714a6a0d83f75733b88f402
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2024-12-03 01:29:36 -0500

    Convertir fecha a YYYY-MM-DD

--- src/components/common/FormatteData.jsx
+++ src/components/common/FormatteData.jsx
@@ -2,3 +2,10 @@
 export const FormatteDecimal = (number, precision) => {
   return parseFloat(number).toFixed(precision)
 }
+export const convertirFechaToYMD = (fechaISO) => {
+  const fecha = new Date(fechaISO)
+  const year = fecha.getFullYear()
+  const month = String(fecha.getMonth() + 1).padStart(2, '0'); // Meses van de 0 a 11
+  const day = String(fecha.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
