ff5175e809bc2b656c0c5550c728b3b8586534ba
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2025-01-08 20:42:45 -0500

    Agregar Padding Botton a la SeccionModel

--- src/components/common/SectionModel.jsx
+++ src/components/common/SectionModel.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const SectionModel = ({title, children}) => {
   return (
     <div className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 pt-3 border border-gray-700 mb-8'>
-	  <div className="flex items-start justify-between rounded-t">
+	  <div className="flex items-start justify-between rounded-t pb-3">
         <h3 className="text-3xl font-semibold text-white">
           {title}
         </h3>
