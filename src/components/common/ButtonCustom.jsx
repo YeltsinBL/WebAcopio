b88ce8e9224577549cfc5e045adbf5ff99722386
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2025-01-10 11:20:09 -0500

    ButtonCustom Agregar Disable

--- src/components/common/ButtonCustom.jsx
+++ src/components/common/ButtonCustom.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const ButtonCustom = ({extraClassName, onClick, name}) => {
+export const ButtonCustom = ({extraClassName, onClick, name, disabled=false}) => {
   return (
-    <button 
+    <button disabled={disabled}
         className={`bg-[#313395] text-white active:bg-gray-700 hover:bg-gray-500 font-bold uppercase text-sm px-4 py-2 rounded-lg
         ${extraClassName}`}
         onClick={onClick}>
