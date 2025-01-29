a295fbdcb8ea31cec24df4aae7b51ab44eb98ef2
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2025-01-29 13:20:09 -0500

    Tabla Popup Ticket Cambio en dimensión

--- src/components/corte/CorteTicketPopup.jsx
+++ src/components/corte/CorteTicketPopup.jsx
@@ -45,7 +45,7 @@ const CorteTicketPopup = ({onShowModel, headers, proveedorId= null}) => {
       <div
         className="justify-center items-center md:flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
       >
-      <div className="relative w-auto my-6 mx-auto md:max-w-3xl lg:max-w-6xl">
+      <div className="relative w-auto my-6 mx-auto md:max-w-3xl lg:max-w-5xl">
         {/*content*/}
         <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-800 outline-none focus:outline-none">
           {/*header*/}
