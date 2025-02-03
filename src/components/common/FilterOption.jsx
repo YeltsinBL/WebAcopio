a7c4d87f1221e5ccf3b6ce6439b0854b93a98f80
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2025-02-03 13:42:27 -0500

    Actualziación del Filter Opcion para mostrar una sola linea

--- src/components/common/FilterOption.jsx
+++ src/components/common/FilterOption.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const FilterOption = ({htmlFor, name, type, placeholder, value, onChange, children}) => {
   return (
     <div className='flex flex-col gap-1 w-1/8'>
-      <label htmlFor={htmlFor} className="text-white">{name}</label>
+      <label htmlFor={htmlFor} className="text-white line-clamp-1">{name}</label>
       {children ? 
         children:
         <input type={type} className='bg-transparent focus:outline-none w-full text-white border border-gray-300 rounded-md px-2 py-1 focus:border-blue-500'
